a52ad5d6b8e9d5027c60f14d7fdb8239e166ce8f
pulkit777exe <pulkitpareek88@gmail.com>
2025-01-13 13:22:08 +0530

    made the discover section responsive

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 export default function Contact(){
     return (
-        <div className="-translate-y-24 ">
+        <div className="">
       <div className="bg-[#ded8c3] text-black py-24">
         <div className="max-w-6xl mx-auto px-4">
             <h1 className="text-5xl font-light tracking-wider mb-8">CONTACT US</h1>
